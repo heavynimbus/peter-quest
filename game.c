132f21bad9373e8ad0b5ab43273e38b48717be84
leo-lamorille <80408160+leo-lamorille@users.noreply.github.com>
2021-05-28 14:25:49 +0200

    feat: add Attack function (#10)

--- game.c
+++ game.c
@@ -37,8 +37,35 @@ int can_deplace(Hero hero){
     return FALSE;
 }
 
-void attack(Hero hero){
-    display_hero(&hero);
+
+//TODO : add la fatigue + gestion de la portée.
+int attack(Box** board, int x1, int y1, int x2, int y2)
+{
+
+    Hero* hero1 = get_hero(board, y1, x1);
+    Hero* hero2 = get_hero(board, y2, x2);
+    int damage = hero1->race->attack;
+    int scope = hero1->race->scope;
+    int len = x1 - x2 + y1 - y2;
+    printf("attack : %d\n", damage);
+    printf("pv du hero2 : %d\n", hero2->hp);
+
+    if (len > scope) {
+        return -1;
+    } else {
+        if (hero2->race->defense < damage) {
+        hero2->hp = hero2->hp - damage + hero2->race->defense;
+        } else {
+            printf("La défense de la cible est trop élevée\n");
+        }
+        return 0;
+    }
+    
+    
+    display_hero(hero2);
+
+    return 0;
+
 }
 
 int can_attack(Hero hero){
@@ -55,55 +82,9 @@ int have_unit(Box** board, int column, char line) {
 }
 
 void choose_move(Box** board, int column, char line, Hero* hero) {
-    /*
-    int res = 0;
-    // check la race, on a le range a la fin
-
-    int range = 1;
-    // deplacement possible
-    // TODO: change range by hero.range hero->race->range
-    for (int k=1; k<range; k++) {
-        if (get_hero(board, column+k, line) == ' ') {
-            printf("Un emplacement est trouvé");
-            res++;
-        }
-        if (get_hero(board, column-k, line) == ' ') {
-            printf("Un emplacement est trouvé");
-            res++;
-        }
-        if (get_hero(board, column, line+k) == ' ') {
-            printf("Un emplacement est trouvé");
-            res++;
-        }
-        if (get_hero(board, column, line-k) == ' ') {
-            printf("Un emplacement est trouvé");
-            res++;
-        }
-    }
-    printf("Emplacements trouvés : %d", res);
-    */
-    // choix du déplacement peut etre dans le deplace
     set_hero(board, column, line, hero);
 }
 
-void play_game(Game game) {
-    char* response1 = malloc(sizeof(char));
-    char* response2 = malloc(sizeof(char));
-    Box** board = game.board;
-    printf("Le joueur indique les coordonnées d'une de ses unités : \n");
-	scanf(" %c%c", response1, response2);
-    
-    //test if unit exist
-    while (have_unit(board, (int)*response2-48, *response1) == FALSE) {
-        printf("Mauvaises coordonnées, veuillez renseigner de bonnes coordonnées.\n");
-        printf("Le joueur indique les coordonnées d'une de ses unités : \n");
-	    scanf(" %c%c", response1, response2);
-    }
-    //char l=response1;
-    //int c=(int)response2-'0';
-    //choose_move(board, c+1, l, get_hero(board, c, l));    
-    display_board(board);
-}
 
 void set_player(Game g, char* username, PlayerType type, int id){
     switch(id){
